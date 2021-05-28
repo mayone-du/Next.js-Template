d0677ad842ad00baef695710a56c6c57daa2b925
mayone-du <cocomayo1201@icloud.com>
2021-05-28 10:40:19 +0900

    Fix add providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import "tailwindcss/tailwind.css";
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
 
 const App = (props: AppProps) => {
+  const client = new ApolloClient({
+    link: createUploadLink({
+      uri: "http://localhost:8000/graphql/",
+    }),
+    cache: new InMemoryCache(),
+  });
+
   return (
     <>
-      <props.Component {...props.pageProps} />
+      <ApolloProvider client={client}>
+        <ThemeProvider>
+          <props.Component {...props.pageProps} />
+        </ThemeProvider>
+      </ApolloProvider>
     </>
   );
 };
