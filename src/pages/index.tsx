faaf67d748653c1eb940ca7c18425729bb3c0178
mayone-du <cocomayo1201@icloud.com>
2021-05-28 10:45:56 +0900

    Add sampel theme-changer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
-import { Layout } from "src/components/Layout";
 import type { NextPage } from "next";
+import { Layout } from "src/components/Layout";
+import { ThemeChanger } from "src/components/ThemeChanger";
 
 const Index: NextPage = () => {
   return (
     <Layout>
       <div className="flex flex-col min-h-screen justify-center items-center">
         <div className="text-5xl">Index Page</div>
+        <ThemeChanger />
       </div>
     </Layout>
   );
