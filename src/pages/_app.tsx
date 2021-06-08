5a031f01ea26d1de7869d933b3063e3b981b2b9f
mayone-du <cocomayo1201@icloud.com>
2021-06-08 22:50:58 +0900

    Add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,27 @@
 import "tailwindcss/tailwind.css";
+import "nprogress/nprogress.css";
 
 import { ApolloProvider } from "@apollo/client";
 import type { NextPageContext } from "next";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import nookies from "nookies";
+import nprogress from "nprogress";
+import { useEffect } from "react";
 import { initializeApollo } from "src/apollo/apolloClient";
+
+nprogress.configure({ showSpinner: false, speed: 400, minimum: 0.25 });
 
 const App = (props: AppProps, context: NextPageContext) => {
   const cookies = nookies.get(context);
-
   const apolloClient = initializeApollo(null, cookies);
+
+  if (process.browser) {
+    nprogress.start();
+  }
+  useEffect(() => {
+    nprogress.done();
+  });
 
   return (
     <div>
