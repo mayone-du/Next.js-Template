c5872eaf1791257d1cd2f8501a5510d01c156d8c
mayone-du <cocomayo1201@icloud.com>
2021-06-26 13:42:21 +0900

    Edit _document, Layout meta

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,6 +11,16 @@ export const Layout: React.FC<Props> = (props) => {
     <div>
       <Head>
         <title>{props.metaTitle}</title>
+        <meta name="description" content="description" />
+        <meta property="og:url" content="https://" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Test Title" />
+        <meta property="og:description" content="Test Description" />
+        <meta property="og:site_name" content="Test SiteName" />
+        <meta property="og:image" content="/images/sample-image.jpg" />
+
+        <meta name="twitter:card" content="Summary Card" />
+        <meta name="twitter:site" content="@mayo1201blog" />
       </Head>
       <Header />
       <main>{props.children}</main>
--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -4,17 +4,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
-        <Head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
-          <meta property="og:url" content="https://" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="Test Title" />
-          <meta property="og:description" content="Test Description" />
-          <meta property="og:site_name" content="Test SiteName" />
-          <meta property="og:image" content="/images/sample-image.jpg" />
-
-          <meta name="twitter:card" content="Summary Card" />
-          <meta name="twitter:site" content="@mayo1201blog" />
-        </Head>
+        <Head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#"></Head>
         <body>
           <Main />
           <NextScript />
