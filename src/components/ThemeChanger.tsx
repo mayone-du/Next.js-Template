15ab6d08b0733105d896589fb438e6242454b833
mayone-du <cocomayo1201@icloud.com>
2021-07-30 09:50:22 +0900

    Add sample headless-ui component, heroicons

--- src/components/ThemeChanger.tsx
+++ src/components/ThemeChanger.tsx
@@ -1,10 +1,14 @@
+import { Switch } from "@headlessui/react";
+import { MoonIcon } from "@heroicons/react/outline";
 import { useTheme } from "next-themes";
 import { memo } from "react";
+import { useState } from "react";
 
 export const ThemeChanger = memo(() => {
+  const [isEnabled, setEnabled] = useState(false);
   const { theme, setTheme } = useTheme();
   const handleLight = () => {
-    setTheme("light");
+    return setTheme("light");
   };
   const handleDark = () => {
     return setTheme("dark");
@@ -14,11 +18,28 @@ export const ThemeChanger = memo(() => {
       <p>The current theme is: {theme}</p>
 
       <div>
+        <div className="py-16">
+          <Switch
+            checked={isEnabled}
+            // eslint-disable-next-line react/jsx-handler-names
+            onChange={setEnabled}
+            className={`${isEnabled ? "bg-teal-900" : "bg-teal-700"}
+          relative inline-flex flex-shrink-0 h-[38px] w-[74px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 bg-gray-400 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+          >
+            <span className="sr-only">Use setting</span>
+            <span
+              aria-hidden="true"
+              className={`${isEnabled ? "translate-x-9" : "translate-x-0"}
+            pointer-events-none inline-block h-[34px] w-[34px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
+            />
+          </Switch>
+        </div>
+        <MoonIcon />
         <button className="border" onClick={handleLight}>
           Light
         </button>
         <button className="border" onClick={handleDark}>
-          Dark
+          dark
         </button>
       </div>
     </div>
