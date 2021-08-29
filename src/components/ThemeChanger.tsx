bcf73be02d43d3db3431196d5a3062da3f32654e
mayone-du <cocomayo1201@icloud.com>
2021-08-29 10:27:15 +0900

    Update ThemeChanger

--- src/components/ThemeChanger.tsx
+++ src/components/ThemeChanger.tsx
@@ -1,48 +1,27 @@
 import { Switch } from "@headlessui/react";
-import { MoonIcon } from "@heroicons/react/outline";
 import { useTheme } from "next-themes";
-import { memo } from "react";
-import { useState } from "react";
+import { memo, useCallback } from "react";
 
 export const ThemeChanger = memo(() => {
-  const [isEnabled, setEnabled] = useState(false);
   const { theme, setTheme } = useTheme();
-  const handleLight = () => {
-    return setTheme("light");
-  };
-  const handleDark = () => {
-    return setTheme("dark");
-  };
-  return (
-    <div>
-      <p>The current theme is: {theme}</p>
+  const handleChangeTheme = useCallback(() => {
+    return theme === "light" ? setTheme("dark") : setTheme("light");
+  }, [theme, setTheme]);
 
-      <div>
-        <div className="py-16">
-          <Switch
-            checked={isEnabled}
-            // eslint-disable-next-line react/jsx-handler-names
-            onChange={setEnabled}
-            className={`${isEnabled ? "bg-teal-900" : "bg-teal-700"}
+  return (
+    <Switch
+      checked={theme === "light"}
+      onChange={handleChangeTheme}
+      className={`${theme === "light" ? "bg-teal-900" : "bg-teal-700"}
           relative inline-flex flex-shrink-0 h-[38px] w-[74px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 bg-gray-400 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-          >
-            <span className="sr-only">Use setting</span>
-            <span
-              aria-hidden="true"
-              className={`${isEnabled ? "translate-x-9" : "translate-x-0"}
+    >
+      <span className="sr-only">Use setting</span>
+      <span
+        aria-hidden="true"
+        className={`${theme === "light" ? "translate-x-9" : "translate-x-0"}
             pointer-events-none inline-block h-[34px] w-[34px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
-            />
-          </Switch>
-        </div>
-        <MoonIcon />
-        <button className="border" onClick={handleLight}>
-          Light
-        </button>
-        <button className="border" onClick={handleDark}>
-          dark
-        </button>
-      </div>
-    </div>
+      />
+    </Switch>
   );
 });
 
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,12 +1,18 @@
 import type { CustomNextPage } from "next";
+import { useSession } from "next-auth/client";
 import { ThemeChanger } from "src/components/ThemeChanger";
 import { Layout } from "src/Layouts";
 
 const IndexPage: CustomNextPage = () => {
+  const [session, isLoading] = useSession();
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <div className="text-5xl">IndexPage</div>
-      <ThemeChanger />
+      <div className="text-center">email:{session?.user?.email ?? "null"}</div>
+      <div className="text-center">isLoading:{isLoading}</div>
+      <div className="py-10">
+        <ThemeChanger />
+      </div>
     </div>
   );
 };
