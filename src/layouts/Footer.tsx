9ed89420a7f4dee071ab65df426ea2a96c659f63
mayone-du <cocomayo1201@icloud.com>
2021-09-12 22:31:29 +0900

    Add footer menus

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link";
 import { memo } from "react";
+import { FOOTER_MENUS } from "src/utils/FOOTER_MENUS";
 
 export const Footer: React.VFC = memo(() => {
   return (
     <footer>
-      <p className="py-6 text-center">copyright&copy;</p>
+      <nav className="bg-gray-400">
+        <ul className="flex">
+          {FOOTER_MENUS.map((menu, index) => {
+            return (
+              <li key={index.toString()}>
+                <Link href={menu.href}>
+                  <a className="block p-2 m-2 border">{menu.href}</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      <p className="py-6 text-center bg-gray-800">copyright&copy;</p>
     </footer>
   );
 });
